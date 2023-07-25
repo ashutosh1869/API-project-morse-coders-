f6fd33e986465a872a62620af914683082d17ccc
ashutosh panigrahi <127655895+ashutosh1869@users.noreply.github.com>
2023-07-25 10:10:59 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -56,7 +56,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=27849f27`);
+            const result = await fetch(`https://omdbapi.com/?s=${searchTerm}&page=1&apikey=27849f27`);
             const movieDetails = await result.json();
             console.log(movieDetails);
             displayMovieDetails(movieDetails);
